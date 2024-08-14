ffcd4a68d9aadf2f8da6e0cb46d89938534ee306
Quynh <101940232+kwspringkle@users.noreply.github.com>
2024-08-14 11:06:41 +0700

    fix database

--- app/models.py
+++ app/models.py
@@ -1,18 +1,7 @@
 from django.db import models
+from goal.models import Goal
 
-class Goal(models.Model): #goal table (goalテーブル)
-    goal_id = models.AutoField(primary_key=True) #goal's id(goalのid)
-    goal_name = models.CharField(max_length=255) #goal's name(goalの名前)
-    start_day = models.DateField() #startday (開始日)
-    finish_day = models.DateField() #finishday(終了日)
-    number_of_task = models.CharField(max_length=255) #number of task (taskの数)
-
-    def __str__(self):
-        return self.goal_name
-
-from .models import Goal #to use goal_id (gaol_idを使うため)
 class Task(models.Model): #task table (taskテーブル) 
-    task_id = models.AutoField(primary_key=True) #task's id (taskのid)
     task_name = models.CharField(max_length=255) #task's name (taskの名前)
     completed = models.BooleanField(default=False) #Is the task completed? (taskが完了しているか)
     goal = models.ForeignKey(Goal, on_delete=models.CASCADE) #ForeignKey from gaol (goalからの外部キー)
@@ -20,8 +9,9 @@ class Task(models.Model): #task table (taskテーブル)
     def __str__(self):
         return self.task_name
 
-from .models import Goal #to use goal_id (gaol_idを使うため)
-from .models import Task #to use task_id (task_idを使うため)
+'''
+from goal.models import Goal #to use goal_id (gaol_idを使うため)
+from app.models import Task #to use task_id (task_idを使うため)
 class Comic(models.Model):  #comic table (comic テーブル)
     comic_id = models.AutoField(primary_key=True) #comic's id (comicのid)
     comic_img = models.CharField(max_length=255) #comic's image (comicの画像)
@@ -32,22 +22,11 @@ class Comic(models.Model):  #comic table (comic テーブル)
 
     def __str__(self):
         return self.comic_img
-
-from .models import Goal #to use goal_id (gaol_idを使うため)
-class TaskByAI(models.Model): #task_by_AI tabel (task_by_AI テーブル) 
-    task_id = models.AutoField(primary_key=True) #task's id (taskのid)
-    task_name = models.CharField(max_length=255) #task's name (taskの名前)
-    number_of_task = models.CharField(max_length=255) #number of task (taskの数)
-    goal = models.ForeignKey(Goal, on_delete=models.CASCADE) #ForeignKey from goal (goalからの外部キー)
-
-    def __str__(self):
-        return self.task_name
-
-from .models import Comic #to use comic_id (comic_idを使うため)
-class Panel(models.Model): #panel table (panle　テーブル)
+class Panel(models.Model): #panel table (panelテーブル)
     panel_id = models.AutoField(primary_key=True) #panel's id (panelのid)
     comic_panel = models.CharField(max_length=255) #panel number (panelの番号)
     panel_img = models.CharField(max_length=255) #panel's image (panelの画像)
     unlocked = models.BooleanField(default=False) #Is the panel opened? (panelが開いているか)
     comic = models.ForeignKey(Comic, on_delete=models.CASCADE) #ForeignKey from comic (comicからの外部キー)
 # Create your models here.
+'''
