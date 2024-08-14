0554369ecc014ecb759d25e740e4f3fad32f54fc
Phu Nguyen <nguyenphu43kun@gmaill.com>
2024-08-14 11:35:02 +0700

    calendar

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+<<<<<<< HEAD
 from .models import Goal #goal table (goalテーブル)
 from .models import Task
 from comic.models import Comic
@@ -13,3 +14,15 @@ admin.site.register(TaskByAI)
 admin.site.register(Panel)
 
 # Register your models here.
+=======
+from goal.models import Goal #goal table (goalテーブル)
+from .models import Task #task table (taskテーブル)
+from goal.models import TaskByAI #task_by_AI tabel (task_by_AI テーブル)
+#from .models import Comic #comic table (comic テーブル)
+#from .models import Panel #panel table (panle　テーブル)
+admin.site.register(Goal)
+admin.site.register(Task)
+admin.site.register(TaskByAI)
+#admin.site.register(Comic)
+#admin.site.register(Panel)
+>>>>>>> 9572eef995bfa3449a74d9d0f555af701a35ddf0
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+<<<<<<< HEAD
 from django.utils import timezone
 
 class Goal(models.Model): #goal table (goalテーブル )
@@ -11,6 +12,10 @@ class Goal(models.Model): #goal table (goalテーブル )
     def __str__(self):
         return self.goal_name
 
+=======
+from goal.models import Goal
+
+>>>>>>> 9572eef995bfa3449a74d9d0f555af701a35ddf0
 class Task(models.Model): #task table (taskテーブル) 
     task_name = models.CharField(max_length=255) #task's name (taskの名前)
     completed = models.BooleanField(default=False) #Is the task completed? (taskが完了しているか)
@@ -20,3 +25,28 @@ class Task(models.Model): #task table (taskテーブル)
 
     def __str__(self):
         return self.task_name
+<<<<<<< HEAD
+=======
+
+'''
+from goal.models import Goal #to use goal_id (gaol_idを使うため)
+from app.models import Task #to use task_id (task_idを使うため)
+class Comic(models.Model):  #comic table (comic テーブル)
+    comic_id = models.AutoField(primary_key=True) #comic's id (comicのid)
+    comic_img = models.CharField(max_length=255) #comic's image (comicの画像)
+    number_of_panel = models.CharField(max_length=255) #number of panel[task] (panel[task]の数)
+    unlocked = models.BooleanField(default=False) #Is the panel opened? (panelが開いているか)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE) #ForeignKey form task (taskからの外部キー)
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE) #ForeignKey from gaol (goalからの外部キー)
+
+    def __str__(self):
+        return self.comic_img
+class Panel(models.Model): #panel table (panelテーブル)
+    panel_id = models.AutoField(primary_key=True) #panel's id (panelのid)
+    comic_panel = models.CharField(max_length=255) #panel number (panelの番号)
+    panel_img = models.CharField(max_length=255) #panel's image (panelの画像)
+    unlocked = models.BooleanField(default=False) #Is the panel opened? (panelが開いているか)
+    comic = models.ForeignKey(Comic, on_delete=models.CASCADE) #ForeignKey from comic (comicからの外部キー)
+# Create your models here.
+'''
+>>>>>>> 9572eef995bfa3449a74d9d0f555af701a35ddf0
