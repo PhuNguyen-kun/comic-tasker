9d0504a65740e943a13d8dd9e8c3cbe78e2124f2
Quynh <101940232+kwspringkle@users.noreply.github.com>
2024-08-16 07:29:25 +0700

    fix routing

--- comic/urls.py
+++ comic/urls.py
@@ -3,6 +3,6 @@ from .views import generate_comic_view, view_comic, mark_task_completed
 
 urlpatterns = [
     path('generate-comic/', generate_comic_view, name='generate_comic'),
-    path('view-comic/<int:comic_id>/', view_comic, name='view_comic'),
+    path('view-comic/<int:goal_id>/', view_comic, name='view_comic'),
     path('mark_task_completed/<int:task_id>/', mark_task_completed, name='mark_task_completed'),
 ]
